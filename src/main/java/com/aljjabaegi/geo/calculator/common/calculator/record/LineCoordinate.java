b2ecd116b07d81023047738c0a4c25c73704f205
GEONLEE <geonlee@neighbor21.co.kr>
2024-06-27 11:41:32 +0900

    [feature] 공통 record package e 변경

--- src/main/java/com/aljjabaegi/geo/calculator/common/calculator/record/LineCoordinate.java
+++ src/main/java/com/aljjabaegi/geo/calculator/common/calculator/record/LineCoordinate.java
@@ -1,14 +1,20 @@
-package com.aljjabaegi.geo.calculator.domain.distance.record;
+package com.aljjabaegi.geo.calculator.common.calculator.record;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 
 /**
- * 선의 버텍스 정보
+ * 선의 좌표 정보
  *
  * @author GEONLEE
  * @since 2024-06-21
  */
+@Schema(description = "선의 좌표 정보")
 public record LineCoordinate(
+        @Schema(description = "선의 좌표 순번", example = "1")
         Long pointSequence,
+        @Schema(description = "경도, X", example = "126.75922354")
         Double longitude,
+        @Schema(description = "위도, Y", example = "35.99849768")
         Double latitude
 ) {
 }
